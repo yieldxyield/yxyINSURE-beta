5f57436ecb973a3a825935b5762b24717e8e899c
yieldplease <73801871+yieldplease@users.noreply.github.com>
2020-11-02 14:07:09 -0500

    beta commit

---
+++ migrations/Initiate.js
@@ -0,0 +1,12 @@
+var yxyINSURE = artifacts.require("./yxyINSURE.sol");
+var YxYi = artifacts.require("./YxYi.sol");
+
+module.exports = async function (deployer) {
+  await deployer.deploy(yxyINSURE);
+  const dfip = await yxyINSURE.deployed();
+
+  await deployer.deploy(YxYi, dfip.address, 0, false, 0);
+  const YxYi = await YxYi.deployed();
+
+  await dfip.approve(YxYi.address, "300000000000000000");
+};
